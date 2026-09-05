e978dc7c54396f765fdf32f2b82d5ee7368b1a3e
Akash Yadav <ay6422853@gmail.com>
2026-09-05 10:05:49 +0530

    Fix admin users API

--- frontend/src/pages/admin/Users.jsx
+++ frontend/src/pages/admin/Users.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./AdminUsers.css";
-import { API } from "../../api";
+import { api } from "../../api";
 
 
 const ROLES = [
